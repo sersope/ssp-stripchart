b164665b2e0009345e4bdc29d25a032429db21f4
Sergio Soriano Peiró <sersope@gmail.com>
2016-04-19 14:58:37 +0200

    Añadido setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ssp_stripchart',
+      version='0.1',
+      description='A GTK+ widget for graphical representation of values over time.',
+      url='https://github.com/sersope/ssp_stripchart',
+      author='Sergio Soriano Peiro',
+      author_email='sersope@gmail.com',
+      license='MIT',
+      py_modules=['ssp_stripchart'],
+      zip_safe=False)
